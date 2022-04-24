c0a1b4d6090406492cf63d0e18bee090d50866ee
odoyonina <odoyoninah@gmail.com>
2022-04-24 14:29:49 +0300

    Patch:add class method to return the user list

--- username.py
+++ username.py
@@ -2,6 +2,7 @@ from collections import UserList, UserString
 
 
 class User:
+
     """
     Class that generates a list of users
     """
@@ -13,31 +14,34 @@ class User:
         """
         ___init___method for user class
         """
+
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
         self.password = password
 
     def save_user(self):
+
         """
         save_user method saves user objects into user_list
         """
+
         User.UserList.append(self)
 
 
     def delete_user(self):
+
+        """
+        delete_user method deletes a saved user from the user_list
+        """
+
         User.UserList.remove(self)
 
-    def __str__(self):
-        return f"{self.firstname} {self.lastname} {self.username} {self.password}"
+    @classmethod
+    def display_users(cls):
 
-    UserList = []
-    def __init__(self, firstname, lastname, username, password):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.username = username
-        self.password = password
+        """
+        method that returns the user list
+        """
 
-        def save_user(self):
-
-        User.UserList.append(self)
+        return cls.UserList
