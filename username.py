526ce0e1045877ebc5f6711fdad2e5349412d2fb
odoyonina <odoyoninah@gmail.com>
2022-04-24 14:11:55 +0300

    Feat:add docstrings class

--- username.py
+++ username.py
@@ -1,11 +1,35 @@
-from collections import UserList
+from collections import UserList, UserString
 
 
 class User:
+    """
+    Class that generates a list of users
+    """
+
+    UserList = []
+
+    def __init__(self, firstname, lastname, username, password):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        User.UserList.append(self)
+
+    def delete_user(self):
+        User.UserList.remove(self)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} {self.username} {self.password}"
+
     UserList = []
     def __init__(self, firstname, lastname, username, password):
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
         self.password = password
-        UserList.append(self)
+
+        def save_user(self):
+
+        User.UserList.append(self)
