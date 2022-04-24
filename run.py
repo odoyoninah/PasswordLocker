bf20fe0013b8f51336f82532c4feb61ef6522dc6
odoyonina <odoyoninah@gmail.com>
2022-04-24 15:56:58 +0300

    create user function

--- run.py
+++ run.py
@@ -3,6 +3,13 @@ from random import choice
 from user import User
 from user import Credentials
 
+def create_user(firstname, lastname, username, password):
+    """
+    Function to create a new user
+    """
+    new_user = User(firstname, lastname, username, password)
+    return new_user
+
 
 
 
