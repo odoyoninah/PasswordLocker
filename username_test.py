a6a00d2df5bcb45a69f9d35019f94b86f9623f94
odoyonina <odoyoninah@gmail.com>
2022-04-24 16:41:31 +0300

    add textusername class

--- username_test.py
+++ username_test.py
@@ -1,3 +1,12 @@
 import unittest
 from user import User
 from user import Credentials
+
+class TestUsername(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+   
